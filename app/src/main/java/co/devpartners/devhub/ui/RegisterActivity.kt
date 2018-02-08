cff11d4366ec55a5cf3ed4b36347dd83e2de2505
artjohn1993 <artjohn1993@gmail.com>
2018-02-08 16:12:44 +0800

    apply animation

--- app/src/main/java/co/devpartners/devhub/ui/RegisterActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/RegisterActivity.kt
@@ -3,7 +3,11 @@ package co.devpartners.devhub.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import co.devpartners.devhub.R
+import com.fujiyuu75.sequent.Animation
+import com.fujiyuu75.sequent.Direction
+import com.fujiyuu75.sequent.Sequent
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_splash.*
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
@@ -14,7 +18,7 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         supportActionBar?.hide()
-
+        Sequent.origin(registerLayout).duration(500).flow(Direction.FORWARD).anim(this, Animation.FADE_IN_DOWN).start()
         nextButton.setOnClickListener {
           startActivity<SpaceTypeActivity>()
         }
