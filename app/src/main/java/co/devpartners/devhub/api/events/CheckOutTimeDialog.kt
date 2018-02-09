ec9aa7b9e5897925578e9784a662ebcd978c921c
artjohn1993 <artjohn1993@gmail.com>
2018-02-09 10:30:05 +0800

    add checklayout

--- app/src/main/java/co/devpartners/devhub/api/events/CheckOutTimeDialog.kt
+++ app/src/main/java/co/devpartners/devhub/api/events/CheckOutTimeDialog.kt
@@ -1,2 +1,27 @@
-package co.devpartners.devhub.api.events 
+package co.devpartners.devhub.api.events
 
+import android.app.Activity
+import android.app.Dialog
+import android.view.Window
+import android.widget.Button
+import co.devpartners.devhub.R
+
+/**
+ * Created by DEVPARTNERS SOFTWARE on 2/9/2018.
+ */
+class CheckOutTimeDialog {
+    lateinit var dialog : Dialog
+    lateinit var select : Button
+    fun show(activity: Activity){
+        dialog = Dialog(activity)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.checkout_layout)
+        select = dialog.findViewById<Button>(R.id.selectCheckOutTime)
+
+        select.setOnClickListener {
+            dialog.hide()
+        }
+        dialog.show()
+    }
+}
