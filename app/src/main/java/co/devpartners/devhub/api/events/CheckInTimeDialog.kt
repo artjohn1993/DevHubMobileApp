a6e38efdd8c779a20569a17485f6dda439173ed2
artjohn1993 <artjohn1993@gmail.com>
2018-02-09 10:02:23 +0800

    add check in time picker

---
+++ app/src/main/java/co/devpartners/devhub/api/events/CheckInTimeDialog.kt
@@ -0,0 +1,27 @@
+package co.devpartners.devhub.api.events
+
+import android.app.Activity
+import android.app.Dialog
+import android.view.Window
+import android.widget.Button
+import co.devpartners.devhub.R
+
+/**
+ * Created by DEVPARTNERS SOFTWARE on 2/9/2018.
+ */
+class CheckInTimeDialog {
+    lateinit var dialog : Dialog
+    lateinit var select : Button
+    fun show(activity: Activity){
+        dialog = Dialog(activity)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.checkin_layout)
+        select = dialog.findViewById(R.id.selectCheckInTime)
+
+        select.setOnClickListener {
+            dialog.hide()
+        }
+        dialog.show()
+    }
+}
