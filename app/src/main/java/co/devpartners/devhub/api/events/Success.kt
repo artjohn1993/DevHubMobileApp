daf6d220586128c9edd157afa1c2b12b6b1a9079
OnyokDawal <junjun061096@gmail.com>
2018-02-09 15:51:47 +0800

    success function

--- app/src/main/java/co/devpartners/devhub/api/events/Success.kt
+++ app/src/main/java/co/devpartners/devhub/api/events/Success.kt
@@ -1,7 +1,28 @@
 package co.devpartners.devhub.api.events
 
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import co.devpartners.devhub.R
+import co.devpartners.devhub.ui.MainActivity
+import org.jetbrains.anko.startActivity
+
+
 /**
  * Created by Dev_03 on 2/9/2018.
  */
-class Success {
+class Success : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.success_layout)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            startActivity<MainActivity>()
+            finish()
+        },3000)
+
+    }
+
 }
