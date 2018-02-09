f11deba976479f4c34f14a3635e177da21bd9c73
OnyokDawal <junjun061096@gmail.com>
2018-02-09 14:27:32 +0800

    adding booking activity

---
+++ app/src/main/java/co/devpartners/devhub/ui/BookingActivity.kt
@@ -0,0 +1,13 @@
+package co.devpartners.devhub.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import co.devpartners.devhub.R
+
+class BookingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking)
+    }
+}
