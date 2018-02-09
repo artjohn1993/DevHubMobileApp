57699c0794ed4ac24263edb76e98acfea2770797
OnyokDawal <junjun061096@gmail.com>
2018-02-09 15:53:21 +0800

    basta sa success ni tanan

--- app/src/main/java/co/devpartners/devhub/ui/BookingActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/BookingActivity.kt
@@ -3,11 +3,18 @@ package co.devpartners.devhub.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import co.devpartners.devhub.R
+import co.devpartners.devhub.api.events.Success
+import kotlinx.android.synthetic.main.activity_booking.*
+import org.jetbrains.anko.startActivity
 
 class BookingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking)
+
+        reserveButton.setOnClickListener {
+            startActivity<Success>()
+        }
     }
 }
