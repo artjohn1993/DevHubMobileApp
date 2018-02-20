0569023fff891d6d58539eedfc5ff9137576797b
OnyokDawal <junjun061096@gmail.com>
2018-02-20 13:56:50 +0800

    asdasd

--- app/src/main/java/co/devpartners/devhub/ui/LoginActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import co.devpartners.devhub.R
+import co.devpartners.devhub.R.layout.activity_userlogin
 import kotlinx.android.synthetic.main.activity_userlogin.*
 import org.jetbrains.anko.startActivity
 
@@ -12,7 +13,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_userlogin)
+        setContentView(activity_userlogin)
         supportActionBar?.hide()
 
         submitButton.setOnClickListener {
