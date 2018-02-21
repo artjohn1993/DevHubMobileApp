08dbc099eb12fb248576efd6c7c3b765b56083f4
OnyokDawal <junjun061096@gmail.com>
2018-02-21 17:05:21 +0800

    animation sa login

--- app/src/main/java/co/devpartners/devhub/ui/LoginActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/LoginActivity.kt
@@ -3,6 +3,8 @@ package co.devpartners.devhub.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import co.devpartners.devhub.R
 import co.devpartners.devhub.R.layout.activity_userlogin
@@ -11,10 +13,16 @@ import org.jetbrains.anko.startActivity
 
 class LoginActivity : AppCompatActivity() {
 
+    var uptodown : Animation? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_userlogin)
         supportActionBar?.hide()
+
+        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown)
+
+        devHubLogo.animation = uptodown
+        loginLayout.animation = uptodown
 
         submitButton.setOnClickListener {
                 startActivity<SpaceTypeActivity>()
