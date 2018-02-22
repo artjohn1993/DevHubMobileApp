f44d80f32f38c907636d013cd110f5b6d590e5d9
artjohn1993 <artjohn1993@gmail.com>
2018-02-22 13:53:00 +0800

    update space selection into fragment

--- app/src/main/java/co/devpartners/devhub/fragments/PrivateFragment.kt
+++ app/src/main/java/co/devpartners/devhub/fragments/PrivateFragment.kt
@@ -1,13 +1,25 @@
 package co.devpartners.devhub.fragments
 
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
 
 import co.devpartners.devhub.R
+import co.devpartners.devhub.api.events.CheckInTimeDialog
+import co.devpartners.devhub.api.events.CustomDatePickerDialog
+import co.devpartners.devhub.api.model.DatePickerType
+import co.devpartners.devhub.ui.BookingActivity
+import kotlinx.android.synthetic.main.activity_private_space.*
+import kotlinx.android.synthetic.main.fragment_private.*
+import org.jetbrains.anko.support.v4.startActivity
+import org.jetbrains.anko.textColor
 
 
 /**
@@ -15,11 +27,38 @@ import co.devpartners.devhub.R
  */
 class PrivateFragment : Fragment() {
 
+    val datepicker = CustomDatePickerDialog()
+    val timepicker = CheckInTimeDialog()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_private, container, false)
-    }
+        val view  = inflater.inflate(R.layout.fragment_private, container, false)
+        val privateNextButton = view.findViewById<Button>(R.id.privateNextButton)
+        val privateSchedule = view.findViewById<Button>(R.id.privateDatePickerButton)
 
-}// Required empty public constructor
+        privateSchedule.setOnClickListener {
+            checkRoomType()
+        }
+        privateNextButton.setOnClickListener {
+            startActivity<BookingActivity>()
+        }
+
+        return view
+    }
+    fun checkRoomType() {
+        when (privateSchedule.selectedItem.toString()) {
+
+            "Length of Stay" -> Toast.makeText(this!!.activity!!, "Please identify your length of stay.", Toast.LENGTH_SHORT).show()
+            "Hourly" -> timepicker.show(this!!.activity!!)
+            "Day" -> datepicker.show(this!!.activity!!, DatePickerType.SINGLE)
+            "Week" -> datepicker.show(this!!.activity!!, DatePickerType.RANGE)
+            else -> datepicker.show(this!!.activity!!, DatePickerType.MULTIPLE)
+        }
+        when(privateSchedule.selectedItem.toString()){
+            "Length of Stay" -> privateDatePickerButton.textColor = Color.RED
+            "Day" ->  privateDatePickerButton.textColor = Color.BLACK
+            "Week" -> privateDatePickerButton.textColor = Color.BLACK
+            "Hourly" -> privateDatePickerButton.textColor = Color.BLACK
+        }
+    }
+}
--- app/src/main/java/co/devpartners/devhub/ui/SpaceTypeActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/SpaceTypeActivity.kt
@@ -8,6 +8,8 @@ import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
 import co.devpartners.devhub.R
+import co.devpartners.devhub.api.events.CustomDatePickerDialog
+import co.devpartners.devhub.api.model.DatePickerType
 import co.devpartners.devhub.fragments.ConferenceFragment
 import co.devpartners.devhub.fragments.OpenFragment
 import co.devpartners.devhub.fragments.PrivateFragment
@@ -15,11 +17,11 @@ import kotlinx.android.synthetic.main.activity_space_type.*
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.startActivity
 import com.basel.DualButton.DualButton
-
+import kotlinx.android.synthetic.main.fragment_conference.*
 
 
 class SpaceTypeActivity : AppCompatActivity() {
-
+    val datepicker = CustomDatePickerDialog()
     private val selectedItem = object : BottomNavigationView.OnNavigationItemSelectedListener {
         override fun onNavigationItemSelected(item: MenuItem): Boolean {
             when (item.getItemId()) {
@@ -57,12 +59,12 @@ class SpaceTypeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_space_type)
         this.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
-        
         navigationBottom.setOnNavigationItemSelectedListener(selectedItem)
         val fragment1 = OpenFragment()
         val fragmentTransaction1 = supportFragmentManager.beginTransaction()
         fragmentTransaction1.replace(R.id.frame, fragment1, "FragmentName")
         fragmentTransaction1.commit()
+
     }
 
 }
