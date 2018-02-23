24fad9d29000d7aa1f9c76d5f4e7c81047ea00a3
OnyokDawal <junjun061096@gmail.com>
2018-02-23 10:02:36 +0800

    update map

--- app/src/main/java/co/devpartners/devhub/api/service/Maps.kt
+++ app/src/main/java/co/devpartners/devhub/api/service/Maps.kt
@@ -25,11 +25,15 @@ class Maps : AppCompatActivity(), OnMapReadyCallback {
     @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        mMap.setMinZoomPreference(6.0f)
-        mMap.setMaxZoomPreference(14.0f)
+        mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.setMinZoomPreference(15.0f)
+        mMap.setMaxZoomPreference(30.0f)
 
-        val devpartners = LatLng(7.0840482, 125.6125157)
+        val devpartners = LatLng(7.0838941, 125.6125771)
         mMap.addMarker(MarkerOptions().position(devpartners).title("Dev Partners"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(devpartners))
+        mMap.animateCamera(CameraUpdateFactory.zoomIn())
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10F), 2000, null)
     }
 }
