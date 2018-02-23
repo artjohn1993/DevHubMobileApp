4f97aadb27047a1e1f5ea730aec1f9bec9eca55e
artjohn1993 <artjohn1993@gmail.com>
2018-02-23 10:11:34 +0800

    update register's  animation

--- app/src/main/java/co/devpartners/devhub/ui/AccountRegisterActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/AccountRegisterActivity.kt
@@ -3,27 +3,26 @@ package co.devpartners.devhub.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.EditText
 import android.widget.Toast
 import co.devpartners.devhub.R
+import com.fujiyuu75.sequent.Direction
+import com.fujiyuu75.sequent.Sequent
 import kotlinx.android.synthetic.main.activity_account_register.*
 import org.jetbrains.anko.startActivity
 
 class AccountRegisterActivity : AppCompatActivity() {
 
-    var uptodown : Animation? = null
-    var fade_in : Animation? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_register)
+        this.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
 
-        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown)
-        fade_in = AnimationUtils.loadAnimation(this,R.anim.fade_in)
-
-        devHubLogo.animation = uptodown
-        bookNowText.animation = fade_in
+        Sequent.origin(registerLayout).duration(400).anim(this,com.fujiyuu75.sequent.Animation.FADE_IN_LEFT).start()
 
         registerButton.setOnClickListener({
 
