3b74e848cb934b98c6c4329d3b8fd6fe0309e289
artjohn1993 <artjohn1993@gmail.com>
2018-02-26 17:11:13 +0800

    add main activity

--- app/src/main/java/co/devpartners/devhub/ui/MainActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/MainActivity.kt
@@ -2,23 +2,11 @@ package co.devpartners.devhub.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import co.devpartners.devhub.R
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
-
-        bookButton.setOnClickListener{
-
-            Handler().postDelayed({
-                startActivity<AccountRegisterActivity>()
-            },450)
-        }
     }
 }
