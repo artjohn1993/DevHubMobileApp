e310b1bc9530704ec6c6bda5e33dd9e6f1f1b8a5
artjohn1993 <artjohn1993@gmail.com>
2018-02-27 15:27:49 +0800

    add informationa activity

---
+++ app/src/main/java/co/devpartners/devhub/ui/InformationActivity.kt
@@ -0,0 +1,14 @@
+package co.devpartners.devhub.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import co.devpartners.devhub.R
+
+class InformationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_information)
+        title = "Information"
+    }
+}
