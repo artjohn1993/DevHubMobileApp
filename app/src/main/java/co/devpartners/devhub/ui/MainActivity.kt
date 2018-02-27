72048fcea2ab36d72751e7a319ece5d3051c9ffe
artjohn1993 <artjohn1993@gmail.com>
2018-02-27 09:10:02 +0800

    add pending listview

--- app/src/main/java/co/devpartners/devhub/ui/MainActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/MainActivity.kt
@@ -6,10 +6,16 @@ import android.support.design.widget.BottomNavigationView
 import android.view.MenuItem
 import co.devpartners.devhub.R
 import co.devpartners.devhub.fragments.*
+import com.github.fabtransitionactivity.SheetLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_space_type.*
+import android.content.Intent
+import android.graphics.Color
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(),SheetLayout.OnFabAnimationEndListener {
+    private val REQUEST_CODE = 1
+
     private val selectedItem = object : BottomNavigationView.OnNavigationItemSelectedListener {
         override fun onNavigationItemSelected(item: MenuItem): Boolean {
             when (item.getItemId()) {
@@ -42,5 +48,27 @@ class MainActivity : AppCompatActivity() {
         val fragmentTransaction1 = supportFragmentManager.beginTransaction()
         fragmentTransaction1.replace(R.id.mainFrame, fragment1, "FragmentName")
         fragmentTransaction1.commit()
+
+        bottom_sheet.setFab(fab)
+        bottom_sheet.setFabAnimationEndListener(this)
+        fab.setBackgroundColor(Color.parseColor("#c67c00"))
+
+        fab.setOnClickListener {
+            bottom_sheet.expandFab()
+        }
     }
+
+
+    override fun onFabAnimationEnd() {
+        val intent = Intent(this, SpaceTypeActivity::class.java)
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE){
+            bottom_sheet.contractFab()
+        }
+    }
+
 }
