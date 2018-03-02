6235a1482822c6d6a7e22c67c5041d01f1a1f739
OnyokDawal <junjun061096@gmail.com>
2018-03-02 13:29:45 +0800

    update

--- app/src/main/java/co/devpartners/devhub/ui/BookingActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/BookingActivity.kt
@@ -1,18 +1,22 @@
 package co.devpartners.devhub.ui
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.view.Gravity
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import co.devpartners.devhub.R
+import co.devpartners.devhub.R.id.*
 import co.devpartners.devhub.api.events.ConfirmationDialog
 import kotlinx.android.synthetic.main.activity_booking.*
-import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.*
+import kotlin.system.exitProcess
 
 class BookingActivity : AppCompatActivity() {
 
@@ -52,7 +56,18 @@ class BookingActivity : AppCompatActivity() {
         }
     }
     fun confirmDialog() {
-        confirm.show(this)
+        reservedSucces()
+    }
+    @SuppressLint("WrongConstant")
+    fun reservedSucces(){
+//        confirm.show(this)
+        alert {
+            title = "Are you sure you want to be book in this space?"
+            positiveButton("Confirm") {
+                backToMain()
+            }
+            negativeButton("Go back") { }
+        }.show()
     }
 //    fun showSuccess(){
 //        val snackbar : Snackbar = Snackbar.make(findViewById(android.R.id.content),"Reserved",Snackbar.LENGTH_LONG)
@@ -62,8 +77,8 @@ class BookingActivity : AppCompatActivity() {
 //        snackbar.show()
 //    }
     fun backToMain(){
-        startActivity<MainActivity>()
-        Toast.makeText(this,"Reserved Successful!",Toast.LENGTH_LONG).show()
-        finish()
+    startActivity<MainActivity>()
+    Toast.makeText(this,"Reserved Successful!", Toast.LENGTH_LONG).show()
+    finish()
     }
 }
